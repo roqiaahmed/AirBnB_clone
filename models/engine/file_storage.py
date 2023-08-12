877b4623727d20fcb73f580f7c1e481d05a31584
akram elgyar <akramelgyar@gmail.com>
2023-08-12 09:49:39 -0700

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -32,6 +32,25 @@ class FileStorage:
         with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file)
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.user import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
         """ Reload the file """
         if os.path.isfile(FileStorage.__file_path):
